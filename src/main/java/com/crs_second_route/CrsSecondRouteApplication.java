f404556b0e0e26909071d557f9e638064a296a2d
Claude Kabayiza <claude.kabayiza@aos.rw>
2022-04-05 17:42:05 +0200

    Initial commit

---
+++ src/main/java/com/crs_second_route/CrsSecondRouteApplication.java
@@ -0,0 +1,13 @@
+package com.crs_second_route;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrsSecondRouteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrsSecondRouteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crs_second_route/CrsSecondRouteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crs_second_route;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrsSecondRouteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
